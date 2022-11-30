8ba69e2c2f2bd1196adeec163a9c726be106f632
sharg2000 <116930449+sharg2000@users.noreply.github.com>
2022-11-29 19:46:03 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-1_iWBf8VQox
-        userPoolClientId:   '',         // e.g. 4eqhppad297o7ospkpec16k9p6
-        region:             ''          // e.g. us-east-1
+        userPoolId:         'us-east-1_iWBf8VQox',         // e.g. us-east-1_iWBf8VQox
+        userPoolClientId:   '4eqhppad297o7ospkpec16k9p6',         // e.g. 4eqhppad297o7ospkpec16k9p6
+        region:             'us-east-1'          // e.g. us-east-1
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
